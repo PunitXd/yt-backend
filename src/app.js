af80c6c86397d6ae5373444d8bc23104800344f8
PunitXd <punitxd.23@gmail.com>
2026-03-28 17:13:41 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -12,4 +12,21 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+/* we cant use agg.get() because our routes are on the diff file 
+and hence we have to use middleware to get the router and we will
+use app.use() */
+
+app.use("/api/v1/users",userRouter)
+//http://localhost:8000/users this link will be opened and it will pass
+//the request to userRouter and then it will check for the route
+//like http://localhost:8000/users/register. 
+
+
+
+
+
 export { app };
