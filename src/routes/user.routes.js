276d66b23a9f09fa64990aa5bb347d16fe427a02
PunitXd <punitxd.23@gmail.com>
2026-03-29 01:46:21 +0530

    fixed all register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,14 +3,14 @@ import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 
-const router = Router();
+const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
             name: "avatar",
             maxCount: 1
-        },
+        }, 
         {
             name: "coverImage",
             maxCount: 1
